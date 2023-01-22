80d4a61c32164bc8adba0bfe23591bad6866726f
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-22 20:55:43 +0900

    [Bronze IV] Title: 기찍 N, Time: 172 ms, Memory: 17108 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n =  Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        for (int i = n; i > 0; i--) {
+            sb.append(i);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
