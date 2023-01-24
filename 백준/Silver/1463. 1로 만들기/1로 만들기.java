59ae4ae285414fb7a05f6ee712c5f9fafff0e99b
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-24 15:57:37 +0900

    [Silver III] Title: 1로 만들기, Time: 84 ms, Memory: 11648 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,16 +1,20 @@
 import java.io.*;
+
 public class Main {
+    public static int third(int n, int count) {
+
+        if (n <= 1) {
+            return count;
+        }
+        return Math.min(third(n / 2, count + 1 + n % 2), third(n / 3, count + 1 + n % 3));
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n+1];
 
-        for (int i = 2; i <= n; i++) {
-                dp[i] = Math.min(dp[i/2]+i%2,dp[i/3]+i%3) +1;
-        }
-        bw.write(dp[n] + "");
+        int result = third(n, 0);
+        bw.write(result + "");
         bw.flush();
         bw.close();
     }
