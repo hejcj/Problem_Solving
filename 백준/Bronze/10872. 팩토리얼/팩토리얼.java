64b2ea02cb88b1cf4baeea7e4c4f4a2d827501ef
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-26 13:13:24 +0900

    [Bronze V] Title: 팩토리얼, Time: 112 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        for (int i = 2; i <= n; i++) {
+                sum *= i;
+        }
+        System.out.println(sum);
+    }
+}
