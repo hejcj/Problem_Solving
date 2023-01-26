6ce50db40313fb49dc7f9fa21daa8c6be6c20a36
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-26 14:17:12 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            sb.append(s.charAt(0)).append(s.charAt(s.length() - 1)).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
