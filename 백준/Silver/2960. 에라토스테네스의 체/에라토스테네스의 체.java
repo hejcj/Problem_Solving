89799a88fb5ff07a651a1b3cb716ff0f3fa0dc43
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-27 15:07:40 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		
+		// k 번째 지워진 수
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int count = 0;
+		
+		boolean[] arr = new boolean[n+1];
+		
+		while (count < k) {
+		
+			for(int i = 2; i <=n ; i++) {
+				
+				if(arr[i] == false) {
+					
+					arr[i] = true;
+					count++;
+					if(count == k) {
+						System.out.println(i);
+						break;
+					}
+					
+					for(int j = i+i; j <= n; j +=i) {
+						if(arr[j] == false) {
+							arr[j] = true;
+							count++;	
+						}
+						if(count == k) {
+							System.out.println(j);
+							break;
+						}
+					}
+				}
+			}
+			
+		}
+		
+	}
+}
