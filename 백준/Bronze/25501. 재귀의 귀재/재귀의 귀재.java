b0a34949c56e30d39a9ea018c65daec579a7697a
hejcj <122426168+hejcj@users.noreply.github.com>
2023-01-30 15:24:35 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 172 ms, Memory: 17988 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -3,34 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
-    public static int[] recursion(String s, int l, int r, int[] result){
-        result[1]++;
-
-        if(l >= r) {
-            result[0] = 1;
-            return result;
-        }
-        else if(s.charAt(l) != s.charAt(r)) {
-            result[0] = 0;
-            return result;
-        }
-        else return recursion(s , l+1, r-1,result);
+    public static int ans;
+    public static int recursion(String s, int l, int r){
+        ans++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s , l+1, r-1);
     }
-    public static int[] isPalindrome(String s) {
-        int[] result = new int[2];
-        result = recursion(s,0,s.length()-1,result);
-        return result;
+    public static int isPalindrome(String s) {
+
+        return recursion(s,0,s.length()-1);
     }
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuffer sb = new StringBuffer();
 
         for (int i = 0; i < t; i++) {
-            int[] result = isPalindrome(br.readLine());
-            sb.append(result[0]).append(" ").append(result[1]).append("\n");
+            ans = 0;
+            sb.append(isPalindrome(br.readLine())).append(" ").append(ans).append("\n");
         }
         System.out.println(sb);
     }
