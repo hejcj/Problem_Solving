45f6a7d9c80313480ca6c7dab40a8be587642689
hejcj <122426168+hejcj@users.noreply.github.com>
2023-02-09 21:43:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 96 ms, Memory: 12152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,54 +1,34 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.StringTokenizer;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         String[] s1 = s.split(" ");
         int n = Integer.parseInt(s1[0]);
         int k = Integer.parseInt(s1[1]);
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
-            linkedList.add(i+1);
+            list.add(i + 1);
         }
-
-        ListIterator<Integer> iterator = linkedList.listIterator();
-
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-
-        while (linkedList.size() != 0) {
-            for (int i = 0; i < k; i++) {
-                if (iterator.hasNext()) {
-                    iterator.next();
-                } else {
-                    while (iterator.hasPrevious()) {
-                        iterator.previous();
-                    }
-                    iterator.next();
-                }
-            }
-            if (iterator.hasPrevious()){
-                if(linkedList.size() != 1) {
-                    sb.append(iterator.previous()).append(", ");
-                } else {
-                    sb.append(iterator.previous());
-                }
-                iterator.remove();
+        int index = 0;
+        while (!list.isEmpty()) {
+            index = (index + (k - 1)) % list.size();
+            sb.append(list.remove(index));
+            if (list.size() == 0) {
+                sb.append(">");
             } else {
-                while (iterator.hasPrevious()) {
-                    iterator.previous();
-                }
+                sb.append(", ");
             }
         }
-        sb.append(">");
-
         System.out.println(sb);
     }
 }
