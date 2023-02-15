502a5713dacbd41a121cd0f212a08e72f2fbc00d
hejcj <122426168+hejcj@users.noreply.github.com>
2023-02-15 17:50:36 +0900

    [Silver V] Title: 나이순 정렬, Time: 480 ms, Memory: 60728 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,35 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        String[] list = new String[N];
+        StringBuilder[] person = new StringBuilder[201];
 
-        for (int i = 0; i < N; i++) {
-            list[i] = br.readLine();
+        for (int i = 0; i < person.length; i++) {
+            person[i] = new StringBuilder();
         }
 
-        Arrays.sort(list, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                StringTokenizer st1 = new StringTokenizer(o1);
-                StringTokenizer st2 = new StringTokenizer(o2);
-                int num1 = Integer.parseInt(st1.nextToken());
-                int num2 = Integer.parseInt(st2.nextToken());
-                return Integer.compare(num1, num2);
-            }
-        });
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            person[Integer.parseInt(s[0])].append(s[0]).append(" ").append(s[1]).append("\n");
+        }
 
         StringBuilder sb = new StringBuilder();
-        for (String a : list) {
-            sb.append(a).append("\n");
+        for (int i = 1; i <= 200; i++) {
+            sb.append(person[i]);
         }
         System.out.println(sb);
     }
