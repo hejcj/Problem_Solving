29a10c9aa437573ffb159c4032f82a99d04e1a2d
hejcj <122426168+hejcj@users.noreply.github.com>
2023-02-20 16:49:10 +0900

    [Silver III] Title: 프린터 큐, Time: 96 ms, Memory: 12172 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
 
@@ -16,13 +17,15 @@ public class Main {
             int N = Integer.parseInt(st.nextToken());
             int M = Integer.parseInt(st.nextToken());
 
+
             StringTokenizer st2 = new StringTokenizer(br.readLine());
             Queue<Integer> queue = new LinkedList<>();
 
             int[] imp = new int[10];
-
+            int max = 0;
             for (int j = 0; j < N; j++) {
                 int d = Integer.parseInt(st2.nextToken());
+                max = Math.max(d, max);
                 imp[d]++;
                 queue.offer(d);
             }
@@ -30,23 +33,22 @@ public class Main {
             int cnt = 0;
             int index = M;
             while (!queue.isEmpty()) {
-                int max = 1;
-                for (int j = 9; j >=1; j--) {
-                    if (imp[j] > 0) {
-                        max = j;
-                        break;
-                    }
-                }
 
                 if (queue.peek() == max) {
                     imp[max]--;
                     queue.poll();
                     cnt++;
                     if (index == 0) {
-                        System.out.println(cnt);
+                        sb.append(cnt).append("\n");
                         break;
                     } else {
                         index--;
+                    }
+                    for (int j = max; j >=1; j--) {
+                        if (imp[j] > 0) {
+                            max = j;
+                            break;
+                        }
                     }
                 } else {
                     queue.offer(queue.poll());
@@ -56,8 +58,8 @@ public class Main {
                         index--;
                     }
                 }
-
             }
         }
+        System.out.println(sb);
     }
 }
