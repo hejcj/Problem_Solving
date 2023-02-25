1d9f714dfc877a12ac2bdc2cd64a4c66434ce534
hejcj <122426168+hejcj@users.noreply.github.com>
2023-02-25 16:33:40 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -5,29 +5,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-
+    // 입력
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     // 지도 크기
     static int n;
-
     // 단지 배열
     static boolean[][] apart;
-
-    // 방문 여부 배열
-    static boolean[][] vis;
 
     // 델타탐색
     static int[] dx = {0, 0, 1, -1};
     static int[] dy = {1, -1, 0, 0};
-
-    // BFS처리할 Queue 케이스가 하나
+    // BFS 처리할 Queue 케이스가 하나
     static Queue<int[]> queue = new LinkedList<>();
-
     // 단지 넓이 배열
     static int[] w = new int[626];
     // 단지 넓이를 정렬 해야함. 빈도수, 버블 ,삽입,
     // 최대 단지수는 ? 25*25 625
     // 빈도수 정렬 선택
+    static int cnt = 0;
 
+    // BFS
     static void bfs() {
         int tempW = 1;
         while (!queue.isEmpty()) {
@@ -35,24 +32,20 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int x = poll[0] + dx[i];
                 int y = poll[1] + dy[i];
-                if(x < 0 || y <0 || x >= n || y >= n) continue;
-                if(vis[x][y] || !apart[x][y]) continue;
+                if(x < 0 || y <0 || x >= n || y >= n || !apart[x][y]) continue;
 
-                vis[x][y] = true;
+                apart[x][y] = false;
                 tempW++;
                 queue.offer(new int[]{x, y});
             }
         }
         w[tempW]++;
     }
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // 입력
+    static void input() throws IOException {
         n = Integer.parseInt(br.readLine());
         apart = new boolean[n][n];
-        vis = new boolean[n][n];
-        int cnt = 0;
-
         // 입력 받기
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
@@ -62,19 +55,8 @@ public class Main {
                 }
             }
         }
-
-        // BFS시작
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (apart[i][j] && !vis[i][j]) {
-                    cnt++;
-                    queue.offer(new int[]{i, j});
-                    vis[i][j] = true;
-                    bfs();
-                }
-            }
-        }
-
+    }
+    static void output() {
         // 출력
         System.out.println(cnt);
         for (int i = 1; i < w.length; i++) {
@@ -85,5 +67,26 @@ public class Main {
                 }
             }
         }
+    }
+    public static void main(String[] args) throws IOException {
+
+        //입력
+        input();
+
+        // BFS 시작
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (apart[i][j]) {
+
+                    apart[i][j] = false;
+                    cnt++;
+                    queue.offer(new int[]{i, j});
+                    bfs();
+                }
+            }
+        }
+        //출력
+        output();
+
     }
 }
