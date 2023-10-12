9dcff67f779a3e210dfb376cf74365d905df577c
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-12 23:24:53 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,12 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        if(n%4 == 0 && n%100 != 0) System.out.println(1);
-        else if (n %400 ==0) System.out.println(1);
-        else System.out.println(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int year = Integer.parseInt(br.readLine());
+        int result = 0;
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) result = 1;
+        System.out.println(result);
     }
 }
