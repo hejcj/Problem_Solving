01928dc0231804723957a18f6fb4ab6272b4b977
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-13 20:37:48 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 일곱 난쟁이의 키의 합이 100
-        int[] small = new int[9];
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] p = new int[101];
+        StringBuilder sb = new StringBuilder();
+        int[] arr = new int[9];
         int sum = 0;
         for (int i = 0; i < 9; i++) {
-            small[i] = Integer.parseInt(br.readLine());
-            sum += small[i];
-            p[small[i]]++;
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-        int idx = 0;
-        for (int i = 1; i < p.length; i++) {
-            if (p[i] > 0) {
-                small[idx++] = i;
-            }
-        }
+        Arrays.sort(arr);
         for (int i = 0; i < 8; i++) {
-            for (int j = i+1; j < 9; j++) {
-                if (i != j) {
-                    if (sum - (small[i] + small[j]) == 100) {
-
-                        StringBuilder sb = new StringBuilder();
-                        for (int k = 0; k < small.length; k++) {
-                            if (k != i && k != j) {
-                                sb.append(small[k]).append("\n");
-                            }
-                        }
-                        System.out.println(sb);
-                        return;
+            for (int j = 1; j < 9; j++) {
+                if(sum - (arr[i]+arr[j]) == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if(k == i || k == j) continue;
+                        sb.append(arr[k]).append("\n");
                     }
+                    System.out.println(sb);
+                    return;
                 }
             }
         }
