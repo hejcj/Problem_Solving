47631b5b41d7f743400f37a471bac664726ac470
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-16 22:06:34 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
-		
-		int a = sc.nextInt();
-		
-		for(int i=0; i<a; i++) {
-			for(int j=0; j<i; j++) {
-				sb.append(" ");
-			}
-			
-			for(int j=2*a-1; j>i*2; j--) {
-				sb.append("*");
-			}
-			sb.append("\n");
-		}
-		sc.close();
-		System.out.println(sb);	
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+
+            for (int j = 0; j < i - 1; j++) {
+                sb.append(" ");
+            }
+
+            for (int j = 0; j < (N - i) * 2 + 1; j++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
 }
