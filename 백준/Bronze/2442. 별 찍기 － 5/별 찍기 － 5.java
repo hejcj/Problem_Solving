78e4f25a15946811b7aa6860f15b141778d7ff12
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-16 22:05:18 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -1,23 +1,21 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
-
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-i-1; j++) {
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 0; j < N - i; j++) {
                 sb.append(" ");
             }
-            for (int j = 0; j < 2*i+1; j++) {
+            for (int j = 0; j < (i - 1) * 2 + 1; j++) {
                 sb.append("*");
             }
             sb.append("\n");
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
