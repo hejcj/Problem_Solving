5954f8d27c781a94f905ea28fee138f718802889
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-16 22:17:59 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 132 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -1,28 +1,29 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer sb = new StringBuffer();
-        int n = Integer.parseInt(br.readLine());
-        int count = 1;
-        boolean flag = false;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int n = N;
 
-        for (int i = 0; i < 2*n-1; i++) {
-            for (int j = 0; j < count-1; j++) {
+        for (int i = 0; i < N * 2 -1 ; i++) {
+
+            if (i < N) n--;
+            else n++;
+
+            for (int j = 0; j < N - n - 1; j++) {
                 sb.append(" ");
             }
-            for (int j = 0; j < 2*(n-count)+1; j++) {
+
+            for (int j = 0; j < n * 2 + 1; j++) {
                 sb.append("*");
             }
-            if(flag) count--;
-            else count++;
-            if(count == n) flag = true;
+
             sb.append("\n");
         }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
