6688583860b082d91ccb527646572ace2ac42cb5
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-18 21:19:50 +0900

    [Silver III] Title: 두 수의 합, Time: 296 ms, Memory: 28424 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -5,25 +5,16 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        int[] arr = new int[1000001];
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
         int x = Integer.parseInt(br.readLine());
-
-        boolean[] is_sum = new boolean[x+1];
-
-        for (int i : arr) {
-            if(i <= x) is_sum[i] = true;
-        }
-        int cnt =0;
-        for (int i = 1; i < (x + 1) / 2; i++) {
-            if((is_sum[i]) && is_sum[x-i]) cnt++;
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (x > num && x - num < 1000001 && arr[x - num] > 0) cnt++;
+            arr[num]++;
         }
         System.out.println(cnt);
     }
