c1c987f29f4a934579ae5ca57abfaa1a4d79c8e9
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-20 23:50:26 +0900

    [Silver II] Title: 에디터, Time: 640 ms, Memory: 128992 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,71 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static final int MX = 600001;
-    public static char[] dat = new char[MX];
-    public static int[] pre = new int[MX];
-    public static int[] nxt = new int[MX];
-
-    static int cura = 1;
-    public static void insert(int addr, char c) {
-        dat[cura] = c;
-        pre[cura] = addr;
-        nxt[cura] = nxt[addr];
-
-        if(nxt[addr] != -1) pre[nxt[addr]] = cura;
-        nxt[addr] = cura;
-        cura++;
-    }
-
-    public static void erase(int addr) {
-        nxt[pre[addr]] = nxt[addr];
-        if(nxt[addr] != -1) pre[nxt[addr]] = pre[addr];
-    }
-    public static void traverse() {
-        StringBuilder sb = new StringBuilder();
-        int cur = nxt[0];
-        while (cur != -1) {
-            sb.append(dat[cur]);
-            cur = nxt[cur];
-        }
-        System.out.println(sb);
-    }
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Arrays.fill(pre, -1);
-        Arrays.fill(nxt, -1);
+        StringBuilder sb = new StringBuilder();
 
         String s = br.readLine();
-        for (int i = 0; i < s.length(); i++) {
-            insert(i, s.charAt(i));
+
+        LinkedList<Character> list = s.chars()
+                .mapToObj(c -> (char) c)
+                .collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
+
+        int cursor = 0;
+        ListIterator<Character> iterator = list.listIterator();
+
+        while (iterator.hasNext()) {
+            iterator.next();
+            cursor++;
         }
-        int t = Integer.parseInt(br.readLine());
-        int cursor = s.length();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char command = st.nextToken().charAt(0);
 
-        for (int i = 0; i < t; i++) {
-            String a = br.readLine();
-
-            if (a.length() == 1) {
-
-                if (a.equals("L")) {
-                    if(pre[cursor] != -1)  cursor = pre[cursor];// cursor 인덱스처럼;
-                } else if (a.equals("B")) {
-                    if(pre[cursor] != -1) {
-                        erase(cursor);
-                        cursor = pre[cursor];
-                    }
-                } else {
-                    if(nxt[cursor] != -1)  cursor = nxt[cursor];
+            if (command == 'L') {
+                if(!iterator.hasPrevious()) continue;
+                cursor--;
+                iterator.previous();
+                continue;
+            }
+            if (command == 'D') {
+                if(iterator.hasNext()) {
+                    cursor++;
+                    iterator.next();
                 }
-            } else {
-                insert(cursor,a.charAt(2));
-                cursor = nxt[cursor];
+                continue;
+            }
+            if (command == 'B') {
+                if(!iterator.hasPrevious()) continue;
+                iterator.previous();
+                iterator.remove();
+                cursor--;
+                continue;
+            }
+
+            if (command == 'P') {
+                iterator.add(st.nextToken().charAt(0));
+                cursor++;
             }
         }
-        traverse();
+
+        for (char c : list) {
+            sb.append(c);
+        }
+        System.out.println(sb);
     }
 }
