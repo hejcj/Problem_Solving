23e280288dde0229900a6a8d1100f97fa603f300
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-23 22:26:33 +0900

    [Silver II] Title: 키로거, Time: 784 ms, Memory: 123008 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -7,39 +7,35 @@ import java.util.ListIterator;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
-            LinkedList<Character> linkedList = new LinkedList<>();
             String s = br.readLine();
-            ListIterator<Character> listIterator = linkedList.listIterator();
-
-            // <<BP<A>>Cd-
+            LinkedList<Character> list = new LinkedList<>();
+            ListIterator<Character> iterator = list.listIterator();
             for (int j = 0; j < s.length(); j++) {
-
-                char c = s.charAt(j);
-                if (c == '<') {
-                    if (listIterator.hasPrevious()) {
-                        listIterator.previous();
-                    }
-                } else if (c == '>') {
-                    if (listIterator.hasNext()) {
-                        listIterator.next();
-                    }
-
-                } else if (c == '-') {
-                    if(listIterator.hasPrevious()) {
-                        listIterator.previous();
-                        listIterator.remove();
-                    }
-
-
-                } else {
-                    listIterator.add(c);
+                if(s.charAt(j) == '<') {
+                    if(iterator.hasPrevious())
+                        iterator.previous();
+                    continue;
                 }
+                if (s.charAt(j) == '>') {
+                    if(iterator.hasNext())
+                        iterator.next();
+                    continue;
+                }
+
+                if(s.charAt(j) == '-') {
+                    if(iterator.hasPrevious()) {
+                        iterator.previous();
+                        iterator.remove();
+                    }
+                    continue;
+                }
+                iterator.add(s.charAt(j));
             }
-            for (char a : linkedList) {
-                sb.append(a);
+            for (char c : list) {
+                sb.append(c);
             }
             sb.append("\n");
         }
