a5b26f4530179dd517b4a3de83c6fb0aa0795640
hejcj <122426168+hejcj@users.noreply.github.com>
2023-10-25 22:54:27 +0900

    [Silver II] Title: 스택 수열, Time: 336 ms, Memory: 27096 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -5,9 +5,6 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 스택에 push하는 순서는 반드시 오름차순을 지키도록 한다.
-        // 스택을 이용해 그 수열을 만들 수 있는지 , 있다면 push와 pop 연산을 수행해야 하는지를 알아낼 수 있다.
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
@@ -16,19 +13,15 @@ public class Main {
 
         while (n-- > 0) {
             int i = Integer.parseInt(br.readLine());
-            if (!stack.isEmpty() && stack.peek() > i) {
+            for (int j = m + 1; j <= i; j++) {
+                stack.push(j);
+                sb.append("+\n");
+            }
+            if(stack.pop() == i) {
+                sb.append("-\n");
+            } else {
                 System.out.println("NO");
                 return;
-            } else if (!stack.isEmpty() && stack.peek() == i) {
-                stack.pop();
-                sb.append("-").append("\n");
-            } else {
-                for (int j = m + 1; j <= i; j++) {
-                    stack.push(j);
-                    sb.append("+").append("\n");
-                }
-                stack.pop();
-                sb.append("-").append("\n");
             }
             m = m > i ? m : i;
         }
