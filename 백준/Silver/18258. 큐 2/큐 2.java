a41f7ed8b556d90751f93e69ee983aa186aa4bf0
hejcj <122426168+hejcj@users.noreply.github.com>
2023-11-07 00:21:15 +0900

    [Silver IV] Title: 큐 2, Time: 1332 ms, Memory: 335644 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,59 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static class Queue {
+        int[] arr;
+        int front;
+        int back;
+
+        public Queue() {
+            int front = 0;
+            int back = 0;
+            this.arr = new int[2000001];
+        }
+        public void push(int n) {
+            arr[back++] = n;
+        }
+        public int pop() {
+            if(empty() == 1) return -1;
+            return arr[front++];
+        }
+
+        public int empty() {
+            if(size() == 0) return 1;
+            return 0;
+        }
+
+        public int size() {
+            return back-front;
+        }
+        public int front() {
+            if(empty() == 1) return -1;
+            return arr[front];
+        }
+
+        public int back() {
+            if(empty() == 1) return -1;
+            return arr[back-1];
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        final int N = Integer.parseInt(br.readLine());
-
-        int[] queue = new int[2000001];
-        int head = 0;
-        int tail = 0;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
+        int N = Integer.parseInt(br.readLine());
+        Queue queue = new Queue();
+        int data = 0;
+        while (N-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
-            switch (st.nextToken()) {
-
-                case "push":
-                    queue[tail++] = Integer.parseInt(st.nextToken());
+            String cmd = st.nextToken();
+            if(st.hasMoreTokens()) {
+                data = Integer.parseInt(st.nextToken());
+            }
+            switch (cmd) {
+                case "push" :
+                    queue.push(data);
                     break;
-
                 case "pop" :
-                    if (head == tail) {
-                        sb.append("-1\n");
-                    } else {
-                        sb.append(queue[head++]).append("\n");
-
-                    }
+                    sb.append(queue.pop()).append("\n");
                     break;
-
                 case "size" :
-                    sb.append(tail-head).append("\n");
+                    sb.append(queue.size()).append("\n");
                     break;
-
-                case "empty":
-                    if(head == tail) sb.append("1\n");
-                    else sb.append("0\n");
+                case "empty" :
+                    sb.append(queue.empty()).append("\n");
                     break;
-
-                case "front":
-
-                    if (head==tail) sb.append("-1\n");
-                    else sb.append(queue[head]).append("\n");
+                case "front" :
+                    sb.append(queue.front()).append("\n");
                     break;
-
-                case "back":
-                    if(head==tail) sb.append("-1\n");
-                    else sb.append(queue[tail-1]).append("\n");
+                case "back" :
+                    sb.append(queue.back()).append("\n");
                     break;
                 default:
                     break;
             }
+
         }
         System.out.println(sb);
     }
